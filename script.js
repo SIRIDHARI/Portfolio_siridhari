d36445bac978ea5f5ea9ff841b8257a30250f5e3
KATARU SIRI DHARI <97428223+SIRIDHARI@users.noreply.github.com>
2024-07-13 11:18:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    var element1 = document.body;
+    const element2=document.getElementById("Nav");
+    element1.classList.toggle("dark-mode");
+    element2.classList.toggle("dark-mode");
+  }
